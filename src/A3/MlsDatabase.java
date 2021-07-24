16877460dc2d4e4642c5556d69d748e3ea97ebfd
devin <75116968+devin-lo@users.noreply.github.com>
2021-07-24 14:54:51 -0400

    Update MlsDatabase.java
    renamed some attributes

--- src/A3/MlsDatabase.java
+++ src/A3/MlsDatabase.java
@@ -14,12 +14,12 @@ public class MlsDatabase {
     public static String CollectionDisclaimer;
 
     // the actual database component is a Map
-    private static volatile Map<UUID, MlsRecord> cache = null;
+    private static volatile Map<UUID, MlsRecord> database = null;
 
     // a maximum cache size needs to be declared - as noted from the Test 1 solution for Q4B
     // set an arbitrary max size of 100
     // must be publicly accessible, final (can't be modified), and static (belongs to class)
-    public final static int MAX_CACHE_SIZE = 100;
+    public final static int MAX_STORAGE_SIZE = 100;
 
     /**
      * getInstance - obtain the reference to the single instance of the Singleton class
@@ -34,7 +34,7 @@ public class MlsDatabase {
                 if (null == instance) {
                     // create the instance and also initialize the cache as a HashMap
                     instance = new MlsDatabase();
-                    cache = new HashMap<UUID, MlsRecord>();
+                    database = new HashMap<UUID, MlsRecord>();
                 }
             }
         }
@@ -51,7 +51,7 @@ public class MlsDatabase {
     }
 
     public MlsRecord searchDatabase(UUID uuid) {
-        return MlsDatabase.cache.get(uuid);
+        return MlsDatabase.database.get(uuid);
     }
 
     /**
@@ -62,8 +62,8 @@ public class MlsDatabase {
      * @return        A boolean to inform the program whether the value was added or not
      */
     public boolean addListing(UUID uuid, MlsRecord listing) {
-        if (MlsDatabase.cache.size() < MlsDatabase.MAX_CACHE_SIZE) {
-            MlsDatabase.cache.put(uuid, listing);
+        if (MlsDatabase.database.size() < MlsDatabase.MAX_STORAGE_SIZE) {
+            MlsDatabase.database.put(uuid, listing);
             return true; // if added or modified
         }
         return false; // if not added due to max cache size reached
@@ -71,7 +71,7 @@ public class MlsDatabase {
 
     public MlsRecord deleteListing(UUID uuid) {
         if (searchDatabase(uuid) != null) {
-            MlsRecord deletedListing = MlsDatabase.cache.remove(uuid);
+            MlsRecord deletedListing = MlsDatabase.database.remove(uuid);
             return deletedListing; // if deleted
         }
         return null; // if nothing deleted
